2d14fd4df42685f088f9fc8a737cc53c4d2aa67e
zhangcaiyan <zhangcaiyanbeyond@gmail.com>
2012-01-11 14:38:21 +0800

    记账系统初始化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Accounting::Application.config.secret_token = 'abeba8a47819e602404243865c92c449886c4b020958c491a169d9c1bde0e288b8a0b98a10c1d53a4050028644b3636c56c81e639bfe421f09d25a09f8190008'
