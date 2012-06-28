bc139a3681efb35d2513f44d7b2cfe1369f76007
zhangcaiyan <zhangcaiyanbeyond@gmail.com>
2012-06-28 15:49:28 +0800

    编辑帐目

--- app/controllers/zhangmus_controller.rb
+++ app/controllers/zhangmus_controller.rb
@@ -12,16 +12,19 @@ class ZhangmusController < ApplicationController
   # GET /zhangmus/new.xml
   def new
     @zhangbens = current_user.zhangbens.order(:position)
-    @peoples = current_user.peoples.order(:position)
     @zhangmu = current_user.zhangmus.new(params[:zhangmu])
     @fenleis = current_user.fenleis.where({shouzhi: @zhangmu.shouzhi}).order(:position)
+    @peoples = current_user.peoples.order(:position)
     @search = current_user.zhangmus.search(params[:search])
     @zhangmus = @search.page(params[:page]).per_page(2).order('created_at DESC')
   end
 
   # GET /zhangmus/1/edit
   def edit
+    @zhangbens = current_user.zhangbens.order(:position)
     @zhangmu = current_user.zhangmus.find(params[:id])
+    @fenleis = current_user.fenleis.where({shouzhi: @zhangmu.shouzhi}).order(:position)
+    @peoples = current_user.peoples.order(:position)
   end
 
   # POST /zhangmus
