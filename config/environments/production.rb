deaff2c4a0060ed468fcceee74056cccd0553d3b
zhangcaiyan <zhangcaiyanbeyond@gmail.com>
2012-10-29 19:53:30 +0800

    去掉压缩文件功能

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Accounting::Application.configure do
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
