e5dd62aed5df90c286ef06e68d6f5f0502d68cae
zhangcaiyan <zhangcaiyanbeyond@gmail.com>
2012-12-26 19:26:02 +0800

    修改记账当前页helper

--- app/helpers/zhangmus_helper.rb
+++ app/helpers/zhangmus_helper.rb
@@ -3,18 +3,30 @@
 module ZhangmusHelper
 
   def current_link(name, url, options={})
+    options[:href] = url
+    content_tag :li, class: current_page_by_full_path?(url) ? "active" : "" do
+      content_tag(:a, name, options)
+    end
+  end
 
+  def current_header_link(name, url, options={})
     options[:href] = url
     content_tag :li, class: current_page?(url) ? "active" : "" do
       content_tag(:a, name, options)
-    end
-
+    end    
   end
 
   protected
 
-  def current_page?(url)
-    url == request.fullpath
+  def current_page_by_full_path?(url)
+    if request.request_parameters.size == 0
+      url == request.fullpath
+    else
+      current_page?(url)
+    end
+    #url == request.fullpath
   end
+
+  
 
 end
